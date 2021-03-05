7c0e1f7562a9b958944a7f471d80746b504e8b85
szyth <syedzia.pro@gmail.com>
2021-03-05 22:42:27 +0530

    Stalker eyes; Particle bg

---
+++ js/init.js
@@ -0,0 +1,89 @@
+$("body").mousemove(function (e) {
+  var eye = $(".eye");
+  var x = eye.offset().left + eye.width() / 2;
+  var y = eye.offset().top + eye.height() / 2;
+  var rad = Math.atan2(e.pageX - x, e.pageY - y);
+  var rot = rad * (180 / Math.PI) * -1 + 400;
+  eye.css({
+    "-webkit-transform": "rotate(" + rot + "deg)",
+    "-moz-transform": "rotate(" + rot + "deg)",
+    "-ms-transform": "rotate(" + rot + "deg)",
+    transform: "rotate(" + rot + "deg)",
+  });
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 563,
+      density: { enable: false, value_area: 8097.23090737089 },
+    },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: false,
+      distance: 1603.412060865523,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 20,
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
