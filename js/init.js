637974cffeb95817153b37c5bd48b21f18850c4b
szyth <syedzia.pro@gmail.com>
2021-03-09 16:19:11 +0530

    polygon face vector added

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ $("body").mousemove(function (e) {
   var x = eye.offset().left + eye.width() / 2;
   var y = eye.offset().top + eye.height() / 2;
   var rad = Math.atan2(e.pageX - x, e.pageY - y);
-  var rot = rad * (180 / Math.PI) * -1 + 400;
+  var rot = rad * (180 / Math.PI) * -1 + 900;
   eye.css({
     "-webkit-transform": "rotate(" + rot + "deg)",
     "-moz-transform": "rotate(" + rot + "deg)",
